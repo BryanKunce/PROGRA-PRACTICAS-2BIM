9ba9900b0e5b69ff6f4a190e7bec9762ebf8066f
Bryan Kunce <129023899+BryanKunce@users.noreply.github.com>
2023-04-13 19:52:11 -0600

    Add files via upload

--- PRACTICA_3.4.ino
+++ PRACTICA_3.4.ino
@@ -1,6 +1,6 @@
 int V2=5;
 double R1=1000.00;
-int V1;
+int V1=7;
 double R2;
 void setup(){
 
@@ -8,9 +8,9 @@ void setup(){
 }
 
 void loop(){
-R2=(((5*1000)/V1)-R2);
+R2=(((5*1000)/7)-R2);
 V2=analogRead(A0);  
-V2=((V1*5)/1023);
+V2=((7*5)/1023);
   
 
 
